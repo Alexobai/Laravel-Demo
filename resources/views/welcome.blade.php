511c530c33c3b2a0ccd8bda2b2e536140469395d
unknown <shhuang@clarkson.edu>
2019-10-09 15:34:40 -0400

    Change frontend text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Helloworld
                 </div>
 
                 <div class="links">
