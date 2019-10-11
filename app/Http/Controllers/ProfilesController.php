601304c5fef305837c50653da4a1addbff65b32a
unknown <shhuang@clarkson.edu>
2019-10-11 09:34:22 -0400

    Change the front end call of follower and following as function name call doesn't work

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -12,24 +12,12 @@ class ProfilesController extends Controller
     {
         $follows = (auth()->user()) ? auth()->user()->following->contains($user->id) :false;
        
-        $postCount = Cache::remember(
-            'count.posts' . $user->id,
-             now()->addSecond(30), 
-             function() use ($user){
-            return $user->posts->count();
-        });
-        $followersCount = Cache::remember(
-            'count.followers' . $user->id,
-             now()->addSecond(30), 
-             function() use ($user){
-            return $user->profile->followers->count();
-        });
-        $followingCount = Cache::remember(
-            'count.following' . $user->id,
-             now()->addSecond(30), 
-             function() use ($user){
-            return $user->following->count();
-        });
+        $postCount = $user->posts->count();
+        
+        $followersCount = $user->profile->followers->count();
+        
+        $followingCount =  $user->following->count();
+        
 
         
 
